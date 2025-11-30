a2aa351926a82876ba9db2cd023cae1d68143270
Artemis Kushner <git@arxdeus.dev>
2025-11-30 16:31:52 +0300

    refactor: remove signals

--- packages/pureflow_flutter/lib/src/adapters.dart
+++ packages/pureflow_flutter/lib/src/adapters.dart
@@ -8,13 +8,13 @@ class ValueUnitListenable<T> implements ValueListenable<T> {
   const ValueUnitListenable._(this._source);
 
   @pragma('vm:prefer-inline')
-  factory ValueUnitListenable(pureflow.Store<T> source) =>
+  factory ValueUnitListenable(pureflow.ValueHolder<T> source) =>
       (_listenables[source] ??= ValueUnitListenable<T>._(source))
           as ValueUnitListenable<T>;
 
   static final _listenables = Expando<ValueUnitListenable<Object?>>();
 
-  final pureflow.Store<T> _source;
+  final pureflow.ValueHolder<T> _source;
 
   @override
   @pragma('vm:prefer-inline')
@@ -34,35 +34,9 @@ class ValueUnitListenable<T> implements ValueListenable<T> {
 // Extensions для удобного преобразования
 // ============================================================================
 
-extension ValueUnitFlutterX<T> on pureflow.Store<T> {
+extension ValueUnitFlutterX<T> on pureflow.ValueHolder<T> {
   /// Создаёт легковесный read-only view как ValueListenable.
   /// Zero overhead - просто делегирует вызовы.
   @pragma('vm:prefer-inline')
   ValueListenable<T> get asListenable => ValueUnitListenable<T>(this);
-}
-
-extension CompositeViewFlutterX<T> on pureflow.Computed<T> {
-  /// CompositeView как ValueListenable (read-only по определению).
-  @pragma('vm:prefer-inline')
-  ValueListenable<T> get asListenable => _CompositeViewListenable<T>(this);
-}
-
-/// Lightweight adapter для CompositeView
-class _CompositeViewListenable<T> implements ValueListenable<T> {
-  const _CompositeViewListenable(this._source);
-
-  final pureflow.Computed<T> _source;
-
-  @override
-  @pragma('vm:prefer-inline')
-  T get value => _source.value;
-
-  @override
-  @pragma('vm:prefer-inline')
-  void addListener(VoidCallback listener) => _source.addListener(listener);
-
-  @override
-  @pragma('vm:prefer-inline')
-  void removeListener(VoidCallback listener) =>
-      _source.removeListener(listener);
 }
