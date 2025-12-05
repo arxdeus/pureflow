cf7a4777e8caed93194b9d95792cb2f03b7ccf5f
Artemis Kushner <git@arxdeus.dev>
2025-12-06 02:44:03 +0300

    fix: lint fix

--- example/flutter_example.dart
+++ example/flutter_example.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
-import 'package:pureflow/pureflow.dart';
 import 'package:pureflow_flutter/pureflow_flutter.dart';
 
 void main() {
